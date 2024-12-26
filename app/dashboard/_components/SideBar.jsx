53b88d60a4fd4984fca37ee8153aae1d7c1b4396
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2024-12-27 01:36:35 +0530

    Update SideBar.jsx

--- app/dashboard/_components/SideBar.jsx
+++ app/dashboard/_components/SideBar.jsx
@@ -1,15 +1,13 @@
 "use client"
 import { Progress } from '@/components/ui/progress';
-import Image from 'next/image'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useContext } from 'react'
-import { HiOutlineHome,HiOutlineSquare3Stack3D,HiOutlineShieldCheck,HiOutlinePower } from "react-icons/hi2";
-;
+import { HiOutlineHome, HiOutlineSquare3Stack3D, HiOutlineShieldCheck, HiOutlinePower } from "react-icons/hi2";
 import { UserCourseListContext } from '@/app/_context/UserCourseListContext';
 
 function SideBar() {
-    const {userCourseList,setUserCourseList}=useContext(UserCourseListContext);
+    const {userCourseList, setUserCourseList} = useContext(UserCourseListContext);
     const Menu=[
         {
             id:1,
@@ -37,32 +35,29 @@ function SideBar() {
         }
     ]
     const path=usePathname();
- return (
-    <div className='fixed h-full md:w-64 p-5 shadow-md'>
-        <h1 className='text-2xl font-bold mb-5'>DASHBOARD</h1>
-        <hr className='my-5' />
-        <ul>
-            {Menu.map((item,index)=>(
-                <Link href={item.path}>
-                    <div className={`flex items-center gap-2 text-gray-600
-                    p-3 cursor-pointer hover:bg-gray-100
-                    hover:text-black rounded-lg mb-3
-                    ${item.path==path&&'bg-gray-100 text-black'}`}> 
-                        <div className='text-2xl'>{item.icon}</div>
-                        <h2>{item.name}</h2>
-                    </div>
-                </Link>
-            ))}
-        </ul>
-
-        <div className='absolute bottom-10 w-[80%]'>
-            <Progress value={(userCourseList?.length/5)*100} />
-            <h2 className='text-sm my-2'>{userCourseList?.length} Out of 5 Course created</h2>
-            <h2 className='text-xs text-gray-500'>Upgrade your plan for unlimted course generate</h2>
-
+    return (
+        <div className='fixed h-full md:w-64 p-5 shadow-md'>
+            <h1 className='text-2xl font-bold mb-5'>DASHBOARD</h1>
+            <hr className='my-5' />
+            <ul>
+                {Menu.map((item,index)=>(
+                    <Link href={item.path}>
+                        <div className={`flex items-center gap-2 text-gray-600
+                        p-3 cursor-pointer hover:bg-gray-100
+                        hover:text-black rounded-lg mb-3
+                        ${item.path==path&&'bg-gray-100 text-black'}`}> 
+                            <div className='text-2xl'>{item.icon}</div>
+                            <h2>{item.name}</h2>
+                        </div>
+                    </Link>
+                ))}
+            </ul>
+            <div className='absolute bottom-10 w-[80%]'>
+                <Progress value={(userCourseList?.length/5)*100} />
+                <h2 className='text-sm my-2'>Total Courses Generated: {userCourseList?.length}</h2>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default SideBar
