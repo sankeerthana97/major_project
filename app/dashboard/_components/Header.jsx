6c977de75cdf8685a567210ac662a546afd75493
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2024-12-27 01:20:01 +0530

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,5 +1,4 @@
 import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,7 +6,9 @@ function Header() {
   return (
     <div className='flex justify-between items-center p-5 shadow-sm'>
        <Link href={'/dashboard'}>
-        <Image src={'/favicon.svg'} width={40} height={40} />
+        <div className='font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-purple-600 to-blue-500 hover:scale-105 transition-transform'>
+          ACADAMY
+        </div>
         </Link>
         <UserButton/>
     </div>
