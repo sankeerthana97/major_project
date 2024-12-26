dd78c040af3e1bf3316a528fd78cf3e35e93fcb0
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2024-12-27 01:13:03 +0530

    Update Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -4,9 +4,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='flex justify-between p-5 shadow-sm'>
+    <div className='flex justify-between items-center p-5 shadow-sm'>
       <Link href={'/'}>
-        <span className='text-2xl font-bold'>ACADAMY</span>
+        <div className='font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-purple-600 to-blue-500 hover:scale-105 transition-transform'>
+          ACADAMY
+        </div>
       </Link>
       <Link href={'/dashboard'}>
         <Button>Get Started</Button>
