49e498a5da835b6bf6e24a011ac4913f79ef37a7
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2024-12-27 00:25:19 +0530

    Update Hero.jsx

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -52,13 +52,13 @@ function Hero() {
 <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
   <div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-2">
 
-    <a className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800" href="#">
+    <a className="group flex flex-col justify-center hover:bg-gray-50 rounded-xl p-4 md:p-7 dark:hover:bg-neutral-800" href="https://major-project-eosin-sigma.vercel.app/dashboard">
       <div className="flex justify-center items-center size-12 bg-blue-600 rounded-xl">
         <svg className="flex-shrink-0 size-6 text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="10" height="14" x="3" y="8" rx="2"/><path d="M5 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-2.4"/><path d="M8 18h.01"/></svg>
       </div>
       <div className="mt-5">
-        <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">25+ templates</h3>
-        <p className="mt-1 text-gray-600 dark:text-neutral-400">Responsive, and mobile-first project on the web</p>
+        <h3 className="group-hover:text-gray-600 text-lg font-semibold text-gray-800 dark:text-white dark:group-hover:text-gray-400">Adaptive Learning</h3>
+        <p className="mt-1 text-gray-600 dark:text-neutral-400">Smart pathways adjust to learning speed.</p>
         <span className="mt-2 inline-flex items-center gap-x-1.5 text-sm text-blue-600 decoration-2 group-hover:underline font-medium">
           Learn more
           <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
