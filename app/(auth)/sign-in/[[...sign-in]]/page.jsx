1ebfc17bd6247771f4fd27f38ece6f363dd0ee3f
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2024-12-27 01:17:02 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -28,12 +28,11 @@ export default function Page() {
         </a>
 
         <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
-          Welcome to Squid 🦑
+          Welcome to ACADAMY
         </h2>
 
         <p className="mt-4 leading-relaxed text-white/90">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-          quibusdam aperiam voluptatum.
+      Transform your learning journey with AI-powered personalized courses. Create your free account today and unlock
         </p>
       </div>
     </section>
@@ -62,12 +61,12 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to Squid 🦑
+            Welcome to ACADAMY
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+          Transform your learning journey with AI-powered personalized courses. Create your free account today and unlock
+
           </p>
         </div>
 
