1f045cbd6ac912a6f6632fc375251cf936988097
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2024-12-30 00:28:18 +0530

    Update UserCourseList.jsx

--- app/dashboard/_components/UserCourseList.jsx
+++ app/dashboard/_components/UserCourseList.jsx
@@ -8,42 +8,41 @@ import CourseCard from './CourseCard'
 import { UserCourseListContext } from '@/app/_context/UserCourseListContext'
 
 function UserCourseList() {
+  const [courseList, setCourseList] = useState([]);
+  const { userCourseList, setUserCourseList } = useContext(UserCourseListContext);
+  const { user } = useUser();
 
-  const [courseList,setCourseList]=useState([]);
-  const {userCourseList,setUserCourseList}=useContext(UserCourseListContext);
-  const {user}=useUser();
+  useEffect(() => {
+    user && getUserCourses();
+  }, [user])
 
-
-  useEffect(()=>{
-    user&&getUserCourses();
-  },[user])
-
-  const getUserCourses=async()=>{
-    const result=await db.select().from(CourseList)
-    .where(eq(CourseList?.createdBy,user?.primaryEmailAddress?.emailAddress))
-    .orderBy(desc(CourseList.id))
+  const getUserCourses = async () => {
+    const result = await db.select().from(CourseList)
+      .where(eq(CourseList?.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .orderBy(desc(CourseList.id))
     setCourseList(result);
     setUserCourseList(result);
   }
+
   return (
     <div className='mt-10'>
-      <h2 className='font-medium text-xl'>My AI Courses</h2>
+      <h2 className='font-medium text-xl text-pink-800'>My AI Courses</h2>
 
       <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {courseList?.length>0?courseList?.map((course,index)=>(
-          <CourseCard course={course} key={index} refreshData={()=>getUserCourses()}/>
+        {courseList?.length > 0 ? courseList?.map((course, index) => (
+          <CourseCard course={course} key={index} refreshData={() => getUserCourses()} />
         ))
-      :
-        [1,2,3,4,5].map((item,index)=>(
-          <div key={index} className='w-full mt-5
-          bg-slate-200 animate-pulse rounded-lg h-[270px]'>
-          </div>
-        ))
-       
-      }
+          :
+          [1, 2, 3, 4, 5].map((item, index) => (
+            <div key={index} className='w-full mt-5
+          bg-pink-200 animate-pulse rounded-lg h-[270px]'>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
 }
 
 export default UserCourseList
+
