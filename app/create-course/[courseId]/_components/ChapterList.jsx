3010d7565bf6622fada7a16db90627f9216a1ed0
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2025-01-03 22:28:33 +0530

    Update ChapterList.jsx

--- app/create-course/[courseId]/_components/ChapterList.jsx
+++ app/create-course/[courseId]/_components/ChapterList.jsx
@@ -5,24 +5,24 @@ import EditChapters from './EditChapters';
 function ChapterList({course, refreshData, edit=true}) {
   return (
     <div className='mt-3'>
-        <h2 className='font-medium text-2xl text-pink-800'>Chapters</h2>
+        <h2 className='font-medium text-2xl text-blue-800'>Chapters</h2>
         <div className='mt-2'>
             {course?.courseOutput?.course?.chapters.map((chapter,index)=>(
-               <div key={index} className='border border-pink-200 p-5 rounded-lg mb-2 flex items-center justify-between hover:bg-pink-50 transition-colors'>
+               <div key={index} className='border border-blue-200 p-5 rounded-lg mb-2 flex items-center justify-between hover:bg-blue-50 transition-colors'>
                 <div className='flex gap-5 items-center'>
-                        <h2 className='bg-pink-600 flex-none h-10 w-10 text-white rounded-full text-center p-2'>{index+1}</h2>
+                        <h2 className='bg-blue-600 flex-none h-10 w-10 text-white rounded-full text-center p-2'>{index+1}</h2>
                         <div className=''>
-                            <h2 className='font-medium text-lg text-pink-700'>
+                            <h2 className='font-medium text-lg text-violet-700'>
                                 {chapter?.name}
                                 {edit && <EditChapters course={course} index={index} refreshData={refreshData} />}
                             </h2>
-                            <p className='text-sm text-pink-500'>{chapter?.about}</p>
-                            <p className='flex gap-2 text-pink-600 items-center'>
-                                <HiOutlineClock className="text-pink-500" /> {chapter?.duration}
+                            <p className='text-sm text-blue-500'>{chapter?.about}</p>
+                            <p className='flex gap-2 text-violet-600 items-center'>
+                                <HiOutlineClock className="text-blue-500" /> {chapter?.duration}
                             </p>
                         </div>
                     </div>
-                    <HiOutlineCheckCircle className='text-4xl text-pink-300 flex-none' />
+                    <HiOutlineCheckCircle className='text-4xl text-violet-300 flex-none' />
                 </div>
             ))}
         </div>
@@ -31,4 +31,3 @@ function ChapterList({course, refreshData, edit=true}) {
 }
 
 export default ChapterList
-
