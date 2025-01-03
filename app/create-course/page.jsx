ccac411d5adfef8fcbe23bdbac755b6a0f1df137
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2025-01-03 22:52:25 +0530

    Update page.jsx

--- app/create-course/page.jsx
+++ app/create-course/page.jsx
@@ -13,6 +13,7 @@ import { CourseList } from '@/configs/schema';
 import uuid4 from 'uuid4';
 import { useUser } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
+
 function CreateCourse() {
   const StepperOptions = [
     {
@@ -76,7 +77,6 @@ function CreateCourse() {
     SaveCourseLayoutInDb(JSON.parse(result.response?.text()));
   }
 
-
   const SaveCourseLayoutInDb=async(courseLayout)=>{
     var id = uuid4();//Course Id
     setLoading(true)
@@ -94,27 +94,26 @@ function CreateCourse() {
     console.log("Finish");
     setLoading(false);
     router.replace('/create-course/'+id)
-
   }
 
   return (
     <div>
       {/* Stepper  */}
       <div className='flex flex-col justify-center items-center mt-10'>
-        <h2 className='text-4xl text-primary font-medium'>Create Course</h2>
+        <h2 className='text-4xl text-blue-600 font-medium'>Create Course</h2>
         <div className='flex mt-10'>
           {StepperOptions.map((item, index) => (
             <div className='flex items-center'>
               <div className='flex flex-col items-center w-[50px] md:w-[100px]'>
                 <div className={`bg-gray-200 p-3 rounded-full text-white
-                ${activeIndex >= index && 'bg-purple-500'}`}>
+                ${activeIndex >= index && 'bg-blue-500'}`}>
                   {item.icon}
                 </div>
                 <h2 className='hidden md:block md:text-sm'>{item.name}</h2>
               </div>
               {index != StepperOptions?.length - 1 &&
                 <div className={`h-1 w-[50px] md:w-[100px] rounded-full lg:w-[170px] bg-gray-300
-             ${activeIndex - 1 >= index && 'bg-purple-500'}
+             ${activeIndex - 1 >= index && 'bg-violet-500'}
              `}></div>}
             </div>
           ))}
@@ -131,8 +130,8 @@ function CreateCourse() {
         <Button disabled={activeIndex==0} 
         variant='outline'
         onClick={()=>setActiveIndex(activeIndex-1)} >Previous</Button>
-       {activeIndex<2&& <Button disabled={checkStatus()} onClick={() => setActiveIndex(activeIndex + 1)}>Next</Button>}
-       {activeIndex==2&& <Button disabled={checkStatus()}  onClick={() => GenerateCourseLayout()}>Generate Course Layout</Button>}
+       {activeIndex<2&& <Button disabled={checkStatus()} onClick={() => setActiveIndex(activeIndex + 1)} className="bg-blue-600 hover:bg-blue-700">Next</Button>}
+       {activeIndex==2&& <Button disabled={checkStatus()}  onClick={() => GenerateCourseLayout()} className="bg-violet-600 hover:bg-violet-700">Generate Course Layout</Button>}
       
       </div>
       </div>
@@ -142,3 +141,4 @@ function CreateCourse() {
 }
 
 export default CreateCourse
+
