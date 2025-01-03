908243597ba3bf39cd2abf6a9fad3edf630a968d
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2025-01-03 22:08:08 +0530

    Update Header.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -6,12 +6,12 @@ function Header() {
   return (
     <div className='flex justify-between items-center p-5 shadow-sm'>
       <Link href={'/'}>
-        <div className='font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-red-950 to-red-900 hover:scale-105 transition-transform'>
+        <div className='font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-blue-600 to-violet-600 hover:scale-105 transition-transform'>
           Learning
         </div>
       </Link>
       <Link href={'/dashboard'}>
-        <Button variant="outline" className="border-red-950 text-red-950 hover:bg-red-100">Get Started</Button>
+        <Button variant="outline" className="border-blue-600 text-blue-600 hover:bg-blue-100">Get Started</Button>
       </Link>
     </div>
   )
