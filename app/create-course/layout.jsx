746c9b4312f8cd6f2f9719b73c3aaeca66b4e19f
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2025-01-03 22:49:25 +0530

    Update layout.jsx

--- app/create-course/layout.jsx
+++ app/create-course/layout.jsx
@@ -4,16 +4,19 @@ import Header from '../dashboard/_components/Header'
 import { UserInputContext } from '../_context/UserInputContext'
 
 function CreateCourseLayout({children}) {
-  const [userCourseInput,setUserCourseInput]=useState([]);
+  const [userCourseInput, setUserCourseInput] = useState([]);
+  
   return (
-    <div>
-      <UserInputContext.Provider value={{userCourseInput,setUserCourseInput}}>
+    <div className="bg-gradient-to-b from-blue-50 to-violet-50 min-h-screen">
+      <UserInputContext.Provider value={{userCourseInput, setUserCourseInput}}>
         <>
-        <Header/>
-        {children}
+          <Header />
+          <main className="text-violet-900">
+            {children}
+          </main>
         </>
       </UserInputContext.Provider>
-      </div>
+    </div>
   )
 }
 
