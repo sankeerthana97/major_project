aee5102e5b4b587dc63224ff74cb907f2dc8741b
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2025-01-03 23:47:14 +0530

    Update ChapterList.jsx

--- app/create-course/[courseId]/_components/ChapterList.jsx
+++ app/create-course/[courseId]/_components/ChapterList.jsx
@@ -1,28 +1,24 @@
 import React from 'react'
 import { HiOutlineCheckCircle, HiOutlineClock } from "react-icons/hi2";
 import EditChapters from './EditChapters';
-
-function ChapterList({course, refreshData, edit=true}) {
+function ChapterList({course,refreshData,edit=true}) {
   return (
     <div className='mt-3'>
-        <h2 className='font-medium text-2xl text-blue-800'>Chapters</h2>
+        <h2 className='font-medium text-xl'>Chapters</h2>
         <div className='mt-2'>
             {course?.courseOutput?.course?.chapters.map((chapter,index)=>(
-               <div key={index} className='border border-blue-200 p-5 rounded-lg mb-2 flex items-center justify-between hover:bg-blue-50 transition-colors'>
+               <div className='border p-5 rounded-lg mb-2 flex items-center justify-between'>
                 <div className='flex gap-5 items-center'>
-                        <h2 className='bg-blue-600 flex-none h-10 w-10 text-white rounded-full text-center p-2'>{index+1}</h2>
+                        <h2 className='bg-primary flex-none h-10 w-10 text-white rounded-full text-center p-2'>{index+1}</h2>
                         <div className=''>
-                            <h2 className='font-medium text-lg text-violet-700'>
-                                {chapter?.name}
-                                {edit && <EditChapters course={course} index={index} refreshData={refreshData} />}
-                            </h2>
-                            <p className='text-sm text-blue-500'>{chapter?.about}</p>
-                            <p className='flex gap-2 text-violet-600 items-center'>
-                                <HiOutlineClock className="text-blue-500" /> {chapter?.duration}
-                            </p>
+                            <h2 className='font-medium text-lg '>{chapter?.name}
+                           {edit&&  <EditChapters course={course} index={index} refreshData={refreshData} />} </h2>
+                            <p className='text-sm text-gray-500 '>{chapter?.about}</p>
+                            <p className='flex gap-2 text-primary items-center'> <HiOutlineClock /> {chapter?.duration}</p>
+
                         </div>
                     </div>
-                    <HiOutlineCheckCircle className='text-4xl text-violet-300 flex-none' />
+                    <HiOutlineCheckCircle className='text-4xl text-gray-300 flex-none' />
                 </div>
             ))}
         </div>
