cf428c18497af8bcdb24135128c75285f55d02db
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2025-01-04 00:42:17 +0530

    Update LoadingDialog.jsx

--- app/create-course/_components/LoadingDialog.jsx
+++ app/create-course/_components/LoadingDialog.jsx
@@ -16,7 +16,7 @@ import Image from 'next/image'
 function LoadingDialog({loading}) {
   return (
     <AlertDialog open={loading}>
-  <AlertDialogContent className="bg-gradient-to-br from-blue-500 via-blue-600 to-violet-600">
+  <AlertDialogContent>
     <AlertDialogHeader>
       <AlertDialogDescription>
         <div className='flex flex-col items-center py-10'>
