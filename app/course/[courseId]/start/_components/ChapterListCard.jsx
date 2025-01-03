1d5b52c985d7ee8cea5460d9199893d99aaa31e8
sankeerthana97 <129671015+sankeerthana97@users.noreply.github.com>
2025-01-03 22:18:29 +0530

    Update ChapterListCard.jsx

--- app/course/[courseId]/start/_components/ChapterListCard.jsx
+++ app/course/[courseId]/start/_components/ChapterListCard.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { HiOutlineClock } from "react-icons/hi2";
+
 function ChapterListCard({chapter,index}) {
   return (
     <div className='grid grid-cols-5 p-4 items-center border-b'>
         <div>
-            <h2 className='p-1 bg-primary w-8 h-8
+            <h2 className='p-1 bg-blue-600 w-8 h-8
              text-white rounded-full text-center'>{index+1}</h2>
         </div>
         <div className='col-span-4'>
             <h2 className='font-medium'>{chapter?.name}</h2>
-            <h2 className='flex items-center gap-2 tex-sm text-primary'> <HiOutlineClock/> {chapter?.duration}</h2>
+            <h2 className='flex items-center gap-2 text-sm text-violet-600'> <HiOutlineClock/> {chapter?.duration}</h2>
         </div>
     </div>
   )
 }
 
 export default ChapterListCard
+
